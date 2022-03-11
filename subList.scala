7b1536aae2d21390578381dc85cacc658aebc1aa
JinDamanee <punnapob.jdk@gmail.com>
2022-03-12 00:17:11 +0700

    fix sublist

--- subList.scala
+++ subList.scala
@@ -9,7 +9,6 @@ object subList {
   def subList(l1:List[Any],l2:List[Any]) : Boolean ={
     if(l1.isEmpty) return true
     if(l2.isEmpty) return false
-    else false
     if(!member(l1.head,l2)) return false
     else return subList(l1.tail,l2)
   }
